05857c05b6a08755a8d7f69861d099d377ae24f3
Alexander Hesser <a.hesser@mxp.de>
2020-12-07 04:52:08 -0800

    Listen for the beforeinstallprompt event

--- assets/javascript/service-worker-pwa.js
+++ assets/javascript/service-worker-pwa.js
@@ -1,31 +1,10 @@
-let installPrompt;
+let deferredPrompt;
 
-const button = document.querySelector('#installButton');
-// hide the button initially
-button.style.display = "none";
-
-window.addEventListener('beforeinstallprompt', e => {
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent the mini-infobar from appearing on mobile
   e.preventDefault();
-  // show the button if it makes sense
-  button.style.display = "block";
-  installPrompt = e;
-});
-
-button.addEventListener('click', () => {
-  if (!installPrompt ) {
-    // The deferred prompt isn't available.
-    return;
-  }
-
-  // Show the install prompt.
-  installPrompt.prompt();
-
-  // Log the result
-  installPrompt.userChoice.then((result) => {
-    console.log('userChoice', result);
-    // Reset the deferred prompt variable, since prompt() can only be called once.
-    installPrompt = null;
-    // Hide the install button.
-    button.style.display = "none";
-  });
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI notify the user they can install the PWA
+  showInstallPromotion();
 });
